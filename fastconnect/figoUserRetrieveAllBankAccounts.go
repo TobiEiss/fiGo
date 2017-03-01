fb4d187f650028f8257338cb19f57cdb19ae8331
tobi <eiss.tobias@googlemail.com>
2017-03-01 11:58:20 +0100

    fix: return interface after retrieveAllBankAccounts

--- fastconnect/figoUserRetrieveAllBankAccounts.go
+++ fastconnect/figoUserRetrieveAllBankAccounts.go
@@ -1,23 +1,24 @@
 package fastconnect
 
 import (
+	"log"
+
 	"github.com/Jeffail/gabs"
 	"github.com/TobiEiss/fiGo"
 )
 
 // RetrieveAllBankAccounts retrieves all bankAccounts
-func RetrieveAllBankAccounts(connection fiGo.IConnection, accessToken string) ([]map[string]interface{}, error) {
-	var transactions []map[string]interface{}
-
-	// get transactions
+func RetrieveAllBankAccounts(connection fiGo.IConnection, accessToken string) (interface{}, error) {
+	// get accounts
 	answerByte, err := connection.RetrieveAllBankAccounts(accessToken)
 	if err != nil {
-		return transactions, err
+		return nil, err
 	}
 
 	// try to get accessToken
+	log.Println(string(answerByte))
 	jsonParsed, err := gabs.ParseJSON(answerByte)
-	accounts, ok := jsonParsed.Path("accounts").Data().([]map[string]interface{})
+	accounts, ok := jsonParsed.Path("accounts").Data().(interface{})
 	if !ok {
 		return accounts, err
 	}
